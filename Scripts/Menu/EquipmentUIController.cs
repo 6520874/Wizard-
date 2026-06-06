77a495411ddfd7519a8722debca65f799f77e1e7
steven.shan <steven.shan@nio.com>
2026-06-07 05:45:06 +0800

    去掉WSAD的逻辑

--- Scripts/Menu/EquipmentUIController.cs
+++ Scripts/Menu/EquipmentUIController.cs
@@ -76,19 +76,19 @@ namespace WitcherGame
                 return;
             }
 
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 MoveMember(-1);
             }
-            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            else if (Input.GetKeyDown(KeyCode.DownArrow) )
             {
                 MoveMember(1);
             }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) )
             {
                 MoveSlot(-1);
             }
-            else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            else if (Input.GetKeyDown(KeyCode.RightArrow) )
             {
                 MoveSlot(1);
             }
