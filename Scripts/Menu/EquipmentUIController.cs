204490bfc0c984bbcc02e68e5fb0e3e2f7af22c1
steven.shan <steven.shan@nio.com>
2026-06-07 06:08:17 +0800

    去掉shift位移

--- Scripts/Menu/EquipmentUIController.cs
+++ Scripts/Menu/EquipmentUIController.cs
@@ -76,23 +76,7 @@ namespace WitcherGame
                 return;
             }
 
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                MoveMember(-1);
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow) )
-            {
-                MoveMember(1);
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow) )
-            {
-                MoveSlot(-1);
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow) )
-            {
-                MoveSlot(1);
-            }
-            else if (Input.GetKeyDown(KeyCode.Return))
+            if (Input.GetKeyDown(KeyCode.Return))
             {
                 CycleSelectedEquipment();
             }
--- Scripts/Player/GeraltController.cs
+++ Scripts/Player/GeraltController.cs
@@ -147,11 +147,12 @@ namespace WitcherGame
                 SetFacingDirection(Mathf.Sign(movement.x));
             }
 
-            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
-            {
-                TryDash(movement.x);
-            }
-            else if (Input.GetKeyDown(KeyCode.E) && !WitcherEquipmentShopTrigger.ShouldBlockPlayerHealInput)
+            // if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+            // {
+            //     TryDash(movement.x);
+            // }
+            // else
+            if (Input.GetKeyDown(KeyCode.E) && !WitcherEquipmentShopTrigger.ShouldBlockPlayerHealInput)
             {
                 TryHeal();
             }
